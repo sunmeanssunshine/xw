acd90723acdb336abd19f06743ecc5f4991b51b0
xuwei <799272872>
2018-09-29 09:04:51 +0800

    EnumSet Demo

--- src/test/java/study/EnumTest.java
+++ src/test/java/study/EnumTest.java
@@ -1,5 +1,7 @@
 package study;
 
+import java.util.EnumSet;
+
 /**
  * Created by xuwei on 2017/6/6.
  */
@@ -7,5 +9,21 @@ public class EnumTest {
     enum Day {
         MONDAY, TUESDAY, WEDNESDAY,
         THURSDAY, FRIDAY, SATURDAY, SUNDAY
+    }
+
+    public static void main(String[] args) {
+        Day[] days = Day.values();
+        for (Day day : days) {
+            System.out.println(day);
+        }
+
+
+        System.out.println("------------------");
+
+        EnumSet<Day> enumSet = EnumSet.range(Day.MONDAY, Day.WEDNESDAY);
+        for (Day day : enumSet) {
+            System.out.println(day);
+        }
+
     }
 }
