79ac49c7b5949c9469516768b72c284f159f79c5
xuwei <799272872>
2018-09-29 15:12:44 +0800

    1.collections remove removeAll 2.File stream

--- src/test/java/study/EnumTest.java
+++ src/test/java/study/EnumTest.java
@@ -1,6 +1,9 @@
 package study;
 
-import java.util.EnumSet;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.*;
 
 /**
  * Created by xuwei on 2017/6/6.
@@ -25,5 +28,33 @@ public class EnumTest {
             System.out.println(day);
         }
 
+    }
+
+    @Test
+    public void test() {
+        Set<Integer> set = Collections.singleton(1);
+        System.out.println(set);
+
+        List<String> list = new ArrayList<>();
+        Collections.addAll(list, "a", "b", "b", "c", "c", "b");
+        list.remove("b");
+        System.out.println(list);
+        list.removeAll(Collections.singleton("c"));
+        System.out.println(list);
+    }
+
+    @Test
+    public void test2() {
+        File[] files = new File(".").listFiles();
+        Arrays.stream(files).filter(File::isFile).map(File::getName).forEach(System.out::println);
+        /**
+         * .gitignore
+         * 1.jpg
+         * 1.txt
+         * 3.jpg
+         * 5.jpg
+         * pom.xml
+         * xw.iml
+         */
     }
 }
